2372fa0031000f3054f7dee484ced98387fb2564
Ahmed Ifhaam <ahmedifhaam1@gmail.com>
2019-09-22 19:30:01 +0530

    Initial Commit with First Excersie

---
+++ MCSD-70-483/Chapter1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace Chapter1
+{
+    public static class Program
+    {
+
+        public static void ThreadMethod()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Thread Proc {0}", i);
+                Thread.Sleep(0);
+            }
+
+        }
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+
+            for(int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main Thread : Do somework!!");
+                Thread.Sleep(0);
+            }
+
+            t.Join();
+        }
+    }
+}
